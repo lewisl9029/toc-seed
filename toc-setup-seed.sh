9c5c65a7c9a398c8b20d607e559850f296531b34
Lewis Liu <develop@lewisl.net>
2015-10-27 19:33:41 -0700

    Patched telehash to add CORS headers
    Added pull and removal of existing image to setup script

--- toc-setup-seed.sh
+++ toc-setup-seed.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -e
+
+sudo docker pull lewisl9029/toc-seed
+sudo docker stop toc-seed
+sudo docker rm toc-seed
+
 sudo docker run -d \
   --name toc-seed --restart always \
   -v /home/toc/toc-seed/config:/toc-seed/config \
